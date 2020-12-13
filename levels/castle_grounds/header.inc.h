45c53aa21fc8b5ba39ee6af3ed1a619fe84266ce
Axoonium <pjozak@gmail.com>
2020-12-13 16:24:51 -0500

    Start on hub (lab) level, add streamed music Music: Static, Interference, Message by 2 Mello

---
+++ levels/castle_grounds/header.inc.h
@@ -0,0 +1,18 @@
+extern const GeoLayout castle_grounds_area_1_geo[];
+extern const GeoLayout castle_grounds_area_1[];
+extern const Collision castle_grounds_area_1_collision[];
+extern const MacroObject castle_grounds_area_1_macro_objs[];
+extern Vtx castle_grounds_dl_Room_mesh_vtx_0[16];
+extern Gfx castle_grounds_dl_Room_mesh_tri_0[];
+extern Vtx castle_grounds_dl_Room_mesh_vtx_1[48];
+extern Gfx castle_grounds_dl_Room_mesh_tri_1[];
+extern Vtx castle_grounds_dl_Table_Render_mesh_vtx_0[88];
+extern Gfx castle_grounds_dl_Table_Render_mesh_tri_0[];
+extern Vtx castle_grounds_dl_Window_mesh_vtx_0[4];
+extern Gfx castle_grounds_dl_Window_mesh_tri_0[];
+
+extern Gfx castle_grounds_dl_Room_mesh[];
+extern Gfx castle_grounds_dl_Table_Render_mesh[];
+extern Gfx castle_grounds_dl_Window_mesh[];
+extern Gfx castle_grounds_dl_material_revert_render_settings[];
+
